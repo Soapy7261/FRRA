188a26c6564f1f178a4c16dbc79486dff6e58a9b
Soapy7261 Official <81933267+Soapy7261@users.noreply.github.com>
2024-08-17 03:19:19 -0400

    localize variables and print what version its running
    I'll work on updating from github later

--- main.lua
+++ main.lua
@@ -1,11 +1,12 @@
 -- Created by Soapy7261 --
+print ("--- FRRA version V1 created by Soapy7261 on github ---")
 
 -- VARIABLES YOU CAN CHANGE --
-EFFICIENCY_THRESHOLD = 80 -- You can change this if you want, but I would also recommend changing reactivity increment to be lower if you're going to increase it, but then you also run the risk of it not being actually fast enough
-ADJUSTMENT_INCREMENT = 5 -- Setting this too high can cause cases where an efficiency of 80 or higher can never be reached, and too low can cause it to be too slow before error level becomes 100%
+local EFFICIENCY_THRESHOLD = 80 -- You can change this if you want, but I would also recommend changing reactivity increment to be lower if you're going to increase it, but then you also run the risk of it not being actually fast enough
+local ADJUSTMENT_INCREMENT = 5 -- Setting this too high can cause cases where an efficiency of 80 or higher can never be reached, and too low can cause it to be too slow before error level becomes 100%
 
 -- CODE --
-LOGIC_ADAPTER = peripheral.find("fusionReactorLogicAdapter")
+local LOGIC_ADAPTER = peripheral.find("fusionReactorLogicAdapter")
 if LOGIC_ADAPTER == nil then
     print ("The computer is not connected to a fusion reactor logic adapter! Exiting...")
     os.exit()
@@ -14,7 +15,7 @@ LOGIC_ADAPTER.adjustReactivity(-100) -- Normalize the environment for possible e
 os.sleep(2) -- race conditions, dont remove this unless you can somehow fix the race condition, oh and make a PR on the github please
 while true do
     if math.floor(LOGIC_ADAPTER.getEfficiency()) < EFFICIENCY_THRESHOLD == true then -- Too low!
-        OldEfficiency = LOGIC_ADAPTER.getEfficiency()
+        local OldEfficiency = LOGIC_ADAPTER.getEfficiency()
         LOGIC_ADAPTER.adjustReactivity(1) -- I could test to see if this is the first run since this isn't needed then, but honestly I dont care.
         if LOGIC_ADAPTER.getEfficiency() < EFFICIENCY_THRESHOLD then -- If this doesn't trigger, then we did it just by incrementing it by 1, unlikely but hey, it can happen!
             if LOGIC_ADAPTER.getEfficiency() < OldEfficiency then -- We were further than before
